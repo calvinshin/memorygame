4294f70f4aecad688c5b59675348376955f9fb09
Calvin Shin <calvin.shin@gmail.com>
2019-07-27 12:39:29 -0700

    This is an update to the gh-pages branch

---
+++ build/precache-manifest.bbcdca59000bf20691f1f00906e7de60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33917050d8951a6a332b02cf5f4117b9",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "a938fb40b36feede7d95",
+    "url": "/memorygame/static/css/main.ebc77f93.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/memorygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a938fb40b36feede7d95",
+    "url": "/memorygame/static/js/main.8c83b1b9.chunk.js"
+  },
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  }
+]);
